95ab0b85f64ff385bfd88bf039f44c0de7fc90f3
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-02-16 23:54:37 -0300

    Projeto criado

---
+++ src/main/java/com/alex/projeto_correios/ProjetoCorreiosApplication.java
@@ -0,0 +1,13 @@
+package com.alex.projeto_correios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCorreiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCorreiosApplication.class, args);
+	}
+
+}
