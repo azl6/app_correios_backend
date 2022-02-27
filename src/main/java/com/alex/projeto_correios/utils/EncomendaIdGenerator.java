70c6fa3f7714b6385abf3310aeaef59d8ee74c64
Alex <alex.rodrigues23997@gmail.com>
2022-02-26 21:12:23 -0300

    Gerando prefixo BR nos ids das encomendas

--- src/main/java/com/alex/projeto_correios/utils/EncomendaIdGenerator.java
+++ src/main/java/com/alex/projeto_correios/utils/EncomendaIdGenerator.java
@@ -14,6 +14,6 @@ public class EncomendaIdGenerator implements IdentifierGenerator {
 
     @Override
     public Serializable generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws HibernateException {
-        return codeGenerator.newCode();
+        return "BR" + codeGenerator.newCode();
     }
 }
