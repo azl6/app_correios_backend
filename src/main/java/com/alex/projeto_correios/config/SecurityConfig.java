e8f4efa45a7714fce81576ca967f303ee7d7d36c
Alex <alex.rodrigues23997@gmail.com>
2022-03-01 00:10:23 -0300

    Implementando autenticação e geração do token JWT - Parte 2

--- src/main/java/com/alex/projeto_correios/config/SecurityConfig.java
+++ src/main/java/com/alex/projeto_correios/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.alex.projeto_correios.config;
 
+import com.alex.projeto_correios.security.JWTAuthenticationFilter;
+import com.alex.projeto_correios.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
     private static final String[] PUBLIC_MATCHERS = {
 
     };
@@ -41,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
